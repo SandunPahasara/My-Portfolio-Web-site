60cf981ec27032055891524c75be3aa28eb055be
Sandun PahasaraWeerasinghe <s.pahasara2001@gmail.com>
2025-06-15 01:48:51 +0530

    Implement responsive navbar with toggle functionality for mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,41 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo" onClick={closeMenu}>
           MyProfile
         </Link>
-        <ul className="nav-menu">
+
+        <div className="menu-icon" onClick={toggleMenu}>
+          {menuOpen ? <>&#10005;</> : <>&#9776;</>} {/* X or ☰ */}
+        </div>
+
+        <ul className={menuOpen ? 'nav-menu active' : 'nav-menu'} id="navMenu">
           <li className="nav-item">
-            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/" className="nav-links" onClick={closeMenu}>Home</Link>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-links">About</Link>
+            <Link to="/about" className="nav-links" onClick={closeMenu}>About</Link>
           </li>
           <li className="nav-item">
-            <Link to="/projects" className="nav-links">Projects</Link>
+            <Link to="/projects" className="nav-links" onClick={closeMenu}>Projects</Link>
           </li>
           <li className="nav-item">
-            <Link to="/contact" className="nav-links">Contact</Link>
+            <Link to="/contact" className="nav-links" onClick={closeMenu}>Contact</Link>
           </li>
         </ul>
       </div>
